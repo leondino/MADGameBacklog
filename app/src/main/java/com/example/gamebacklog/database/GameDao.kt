e79ebf03e642e8a08b05fc98da0c030898804916
Léon Imanuel <leondino0309@gmail.com>
2020-04-23 12:33:41 +0200

    -Added Dependencies -Added class for AddActivity -Added Game object en GameDao

---
+++ app/src/main/java/com/example/gamebacklog/database/GameDao.kt
@@ -0,0 +1,25 @@
+package com.example.gamebacklog.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.gamebacklog.model.Game
+
+@Dao
+interface GameDao {
+
+    @Query("SELECT * FROM gameTable")
+    fun getAllGames(): LiveData<List<Game>>
+
+    @Insert
+    suspend fun insertGame(game:Game)
+
+    @Delete
+    suspend fun deleteGame(game: Game)
+
+    @Query("DELETE FROM gameTable")
+    suspend fun deleteAllGames()
+
+}
